a678eb9aaedec8b2809bf214d4e85bb44a0b2754
ralichow <87010003+ralichow@users.noreply.github.com>
2023-10-19 02:28:09 +1100

    Add files via upload

---
+++ Javascript/Script.js
@@ -0,0 +1,30 @@
+/*
+function addNumbers(N1,N2){
+    const total = N1 + N2;
+    return total;
+
+}
+
+const addedTotal = addNumbers(1,2);
+
+console.log("Hello");
+*/
+
+/*
+
+function addNumbers(Name, ...numbers){
+
+    let total= 0;
+
+    for(index in numbers){
+
+        total = total + numbers[index]
+      
+    }
+    return `${Name}: the total is ${total}`;
+
+} 
+
+const newTotal = addNumbers("RAFI",1,4,5,3);
+console.log(newTotal);
+/*
